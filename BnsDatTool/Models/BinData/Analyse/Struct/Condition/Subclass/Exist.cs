0359b30e43bd0d339a1d598c5ab5a643980b0eff
xylia <xyliaup@qq.com>
2023-05-11 23:14:16 +0800

    source code

---
+++ BnsDatTool/Models/BinData/Analyse/Struct/Condition/Subclass/Exist.cs
@@ -0,0 +1,22 @@
+﻿
+using Xylia.Attribute;
+
+namespace Xylia.bns.Modules.DataFormat.Analyse.Condition
+{
+	public class Exist : ValidCondition
+	{
+		#region Constructor
+		public Exist(bool Flag) => this.Flag = Flag;
+
+		/// <summary>
+		/// 是否存在
+		/// </summary>
+		public bool Flag = true;
+		#endregion
+
+
+		#region Functions
+		protected override bool IsMeet(IHash Hash, bool ExistTarget) => (Flag && ExistTarget) || (!Flag && !ExistTarget);
+		#endregion
+	}
+}
---
+++ BnsDatTool/Models/BinData/Analyse/Struct/Condition/Subclass/NonOut.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xylia.Attribute;
+
+namespace Xylia.bns.Modules.DataFormat.Analyse.Condition
+{
+	public class NonOut : Condition
+	{
+		#region Functions
+		protected override bool IsMeet(IHash Hash, bool ExistTarget) => false;
+		#endregion
+	}
+}
